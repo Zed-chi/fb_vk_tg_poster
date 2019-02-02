b00fb5c3c1cb5fb38ef5db184dd7ffd59943a123
AlexE <zed-chi@mail.ru>
2019-02-03 00:41:28 +0300

    moved args

--- post_facebook.py
+++ post_facebook.py
@@ -3,30 +3,27 @@ import requests
 from dotenv import load_dotenv
 import utils
 
-
 FB_API = "https://graph.facebook.com/"
-FB_GROUP_ID = os.getenv("fb_group_id")
-TOKEN = os.getenv("fb_token")
 
 
 def post_message_to_group(message=None, group_id=None):
     if not group_id or not message:
         return None
     params = {
-        "access_token": TOKEN,
+        "access_token": token,
         "message": message,
     }
-    url = "{}{}{}".format(FB_API, FB_GROUP_ID, "/feed")
+    url = "{}{}{}".format(FB_API, group_id, "/feed")
     res = requests.post(url, params=params)
     return res.ok
 
 
-def post_image_to_group(message=None, id=None):
+def post_image_to_group(message=None, id=None, token=None, group_id=None):
     if not id or not message:
         return None
-    url = "{}{}{}".format(FB_API, FB_GROUP_ID, "/feed")
+    url = "{}{}{}".format(FB_API, group_id, "/feed")
     params = {
-        "access_token": TOKEN,
+        "access_token": token,
         "message": message,
         "attached_media[0]": str({
             "media_fbid": id
@@ -36,12 +33,12 @@ def post_image_to_group(message=None, id=None):
     return res
 
 
-def upload_image(image_content):
-    url = "{}{}{}".format(FB_API, FB_GROUP_ID, "/photos")
+def upload_image(image_content, token=None, group_id=None):
+    url = "{}{}{}".format(FB_API, group_id, "/photos")
     files = {"file": image_content}
     params = {
         "caption": "test",
-        "access_token": TOKEN,
+        "access_token": token,
         "published": False,
     }
     res = requests.post(url, params=params, files=files)
@@ -49,12 +46,14 @@ def upload_image(image_content):
 
 
 def post(text_path=None, image_path=None):
+    FB_GROUP_ID = os.getenv("fb_group_id")
+    TOKEN = os.getenv("fb_token")
     image_file = utils.get_file_content(image_path, file_type="image")
     text = utils.get_file_content(text_path)
-    upload = upload_image(image_file)
+    upload = upload_image(image_file, token=TOKEN, group_id=FB_GROUP_ID)
     if upload.ok:
         id = upload.json()["id"]
-        post_image_to_group(message=text, id=id)
+        post_image_to_group(message=text, id=id, token=TOKEN, group_id=FB_GROUP_ID)
     else:
         print(upload.status_code)
 
@@ -62,6 +61,6 @@ def post(text_path=None, image_path=None):
 if __name__ == "__main__":
     load_dotenv()
     args = utils.get_args()
-    text_path = args["path_to_text"] or exit()
-    image_path = args["path_to_image"] or exit()
+    text_path = args.path_to_text or exit()
+    image_path = args.path_to_image or exit()
     post(text_path=text_path, image_path=image_path)
